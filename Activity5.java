333f683d4ca47ce0f17cd93da4a41a4463fb73cb
DESKTOP-I9SF0ON\User <gargarja@students.nu-clark.edu.ph>
2024-10-10 09:58:23 +0800

    activity5555

---
+++ Activity5.java
@@ -0,0 +1,41 @@
+public class Activity5 {
+    public static void main(String [] args) {
+
+        //1. Byte Data Type
+        int jlg_byteVar = 321;
+        System.out.println ("Byte : " + jlg_byteVar);
+
+        //2. Short Data Type
+        int jlg_shortVar = 43567;
+        System.out.println ("Short : " + jlg_shortVar);
+
+        //3. Int Data Type
+        int jlg_intVar = 2435546;
+        System.out.println ("Int : " + jlg_intVar);
+
+        //4. Long Data Type
+        int jlg_longVar = 242567543;
+        System.out.println ("Long : " + jlg_longVar);
+
+        //5. Float Data Type
+        float jlg_floatVar = 7.2345354f;
+        System.out.println ("Float : " + jlg_floatVar);
+
+        //6. Double Data Type
+        double jlg_doubleVar = 17.32423243543;
+        System.out.println ("Double : " + jlg_doubleVar);
+
+        //7. Char Data Type
+        char jlg_charVar = 'L';
+        System.out.println ("Char : " + jlg_charVar);
+
+        //8. Boolean Data Type
+        boolean jlg_boolVar = false;
+        System.out.println ("Boolean : " + jlg_boolVar);
+
+        System.out.println("Hello\nWorld");
+        System.out.println("\tMiss ko na siya");
+        System.out.println("\\Backslash");
+
+    }
+}
